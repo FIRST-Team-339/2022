b0500d76d003dba491ebdd4c268991dde0d2b9ce
Robert Brown <rjbrown124@comcast.net>
2022-02-12 14:19:20 -0500

    feat(BallCounter): Made ball counter class and static integer

---
+++ src/main/java/frc/Utils/ballcounter/BallCounter.java
@@ -0,0 +1,21 @@
+// ===================================================================
+// FILE NAME: BallCounter.java (Team 339 - Kilroy)
+//
+// CREATED ON: Feb 12, 2022
+// CREATED BY: Jacob Fisher
+// MODIFIED ON: Feb 12, 2022
+// MODIFIED BY: Jacob Fisher
+// ABSTRACT:
+// This file contains all of the global definitions for the
+// ball counter related items in the system
+// (probably just the one BallCounter integer)
+//
+// NOTE: Please do not release this code without permission from
+// Team 339.
+// ====================================================================
+package frc.Utils.ballcounter;
+
+public class BallCounter
+    {
+    public static int BallCount = 0;
+    }
