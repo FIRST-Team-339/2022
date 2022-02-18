baa27638cc92ff292338f95897ceeaa64a5ea573
Robert Brown <rjbrown124@comcast.net>
2022-02-17 19:51:43 -0500

    Added intested code to ballHandler.java

--- src/main/java/frc/HardwareInterfaces/BallHandler.java
+++ src/main/java/frc/HardwareInterfaces/BallHandler.java
@@ -2,10 +2,54 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// ---------------------------------------
+
+// Stores the state machine for the launch
+
 package frc.HardwareInterfaces;
+
+import frc.Hardware.Hardware;
 
 /** Add your docs here. */
 public class BallHandler
     {
 
+    // Where code gets initalized
+    public static void ballHandlerInit()
+    {
+
     }
+
+    // Where code gets called periodically
+    public static void ballHandlerPeriodic()
+    {
+        // ---------------------------------------
+        // Keep Code Commented Until Ready To Test
+        // ---------------------------------------
+        // State currentState = State.EJECTING;
+        // switch (currentState)
+        // {
+        // case EJECTING:
+        // Hardware.intakeMotor.set(possibleIntakeMotorEjectingSpeed);
+        // break;
+        // case INTAKE:
+        // if (Hardware.intakeMotor.get() > stillIntakeMotor)
+        // {
+
+        // }
+        // break;
+        // default:
+        // System.out.println("!!!SOMETINGS BROKEN!!!");
+        // break;
+
+    }
+
+    }
+
+// public static enum State
+// {
+// EJECTING, INTAKE;
+// }
+
+// public static double possibleIntakeMotorEjectingSpeed = -1.0;
+// public static double stillIntakeMotor = 0.0;
